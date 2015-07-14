659eaed1c41e518dd3089d6e8e23c3abe0881853
stonedong <yishuiliunian@gmail.com>
2015-07-14 22:33:16 +0800

    no message

---
+++ initframe.m
@@ -0,0 +1,18 @@
+// init
+// Initialization method
+//
+// IDECodeSnippetCompletionPrefix: initframe
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: 3FF72E6A-3950-4D15-8DC2-306D0F5E123F
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+- (instancetype) initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self) {
+        return self;
+    }
+    <#init content#>
+    return self;
+}
