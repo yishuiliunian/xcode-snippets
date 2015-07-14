da7a76557d0ce83090619df3fd2cd02539f27baa
stonedong <yishuiliunian@gmail.com>
2015-07-14 22:38:33 +0800

    no message

---
+++ forint.m
@@ -0,0 +1,11 @@
+// For Statement with int i++
+// Execute code for each value in a sequence of values. with int i
+//
+// IDECodeSnippetCompletionPrefix: forint
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 167AF381-F451-4535-900D-EXB65C99DB1F
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
+// IDECodeSnippetVersion: 1
+for (int i = 0; i < <#max#>; i++) {
+    <#statements#>
+}
