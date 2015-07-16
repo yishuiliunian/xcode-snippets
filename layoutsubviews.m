a66bc402b89e70598efae3c4f1efa510fb80552a
stonedong <yishuiliunian@gmail.com>
2015-07-16 22:31:37 +0800

    layoutsubviews

---
+++ layoutsubviews.m
@@ -0,0 +1,13 @@
+// LayoutSubviews implitation
+// Execute code for each value in a sequence of values. with int i
+//
+// IDECodeSnippetCompletionPrefix: layoutsubviews
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: 167BFD81-F451-4535-900D-EXB65C99DB1F
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
+// IDECodeSnippetVersion: 1
+- (void) layoutSubviews
+{
+    [super layoutSubviews];
+    <#content#>
+}
