4eeacddc6ceb85c980b96ebaf9f31401580d59cc
dongzhao <yishuiliunian@gmail.com>
2015-07-17 14:06:20 +0800

    Imported user code snippets

---
+++ for_i++.m
@@ -0,0 +1,12 @@
+// for i++
+// 
+//
+// IDECodeSnippetCompletionPrefix: foriiii
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 0921D12F-B36B-47E4-989B-B59F2B32CC84
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+for (int i = 0; i < <#max#>; i++) {
+        <#content#>
+    }
